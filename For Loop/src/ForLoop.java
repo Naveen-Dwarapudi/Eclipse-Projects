7b36782ee4e598cbfcdc7e16cb03ddb4fa04c361
Naveen-Dwarapudi <dbsnaveen@gmail.com>
2022-05-25 14:47:49 +0530

    first commit

---
+++ For Loop/src/ForLoop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		for (int i=10; i>=0; i--) {
+			System.out.println(i + " iteration");
+		}
+		
+		System.out.println("Loop Ends");
+
+	}
+
+}
