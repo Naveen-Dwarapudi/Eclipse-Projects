c2907a6f086d54c8fe4d4cec76190b3eef816edd
Naveen-Dwarapudi <dbsnaveen@gmail.com>
2022-05-25 14:46:35 +0530

    first commit

---
+++ Arrays/src/Arrays.java
@@ -0,0 +1,13 @@
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] cars = {"Benz", "Toyota", "Tesla", "Audi"};
+		
+		for(int i=0; i<cars.length; i++ ) {
+			System.out.println(cars[i]);	
+		}
+	}
+
+}
