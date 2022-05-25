2f7338f6ed5188836f450d40473c8683d19d181b
Naveen-Dwarapudi <dbsnaveen@gmail.com>
2022-05-25 14:52:37 +0530

    first commit

---
+++ Wrapper Classes/src/WrapperClass.java
@@ -0,0 +1,15 @@
+
+public class WrapperClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Boolean a = true;
+		Character b = '@';
+		Integer c = 123;
+		Double d = 3.14;
+		String e = "Naveen";
+		
+		
+	}
+
+}
