895f8365e13e472273cae6f0f2737ffae0a54103
Naveen-Dwarapudi <dbsnaveen@gmail.com>
2022-05-25 14:49:52 +0530

    first commit

---
+++ String Methods/src/StringMethods.java
@@ -0,0 +1,25 @@
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "   Naveen   ";
+		
+		//boolean result = name.equals("Naveen");
+		//boolean result = name.equalsIgnoreCase("naveen");
+		//int result = name.length();
+		//char result = name.charAt(1);
+		//int result = name.indexOf("a");
+		//boolean result = name.isEmpty();
+		//String result = name.toUpperCase();
+		//String result = name.toLowerCase();
+		//String result = name.trim();
+		String result = name.replace('e', 'a');
+		
+		
+		System.out.println(result);
+
+	}
+
+}
