84d472dbe3668baf35423ed5a74b001ae00d9240
Naveen-Dwarapudi <dbsnaveen@gmail.com>
2022-05-25 14:45:31 +0530

    first commit

---
+++ Array Lists/src/ArrayList.java
@@ -0,0 +1,20 @@
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+		// Array List = a resizable array.
+		ArrayList<String> food = new ArrayList<String>();
+		
+		food.add("Pizza");
+		food.add("hamburger");
+		food.add("hotdog");
+		
+		food.set(0, "sushi");
+		food.remove(2);
+		food.clear();
+		
+		for 
+
+	}
+
+}
